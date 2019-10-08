e57792730b413627c6879ac9f6110cded00907f3
acbarboza <54550664+acbarboza@users.noreply.github.com>
2019-10-08 01:00:41 +0100

    Add files via upload

---
+++ Traffic lights/index.js
@@ -0,0 +1,11 @@
+let cor ="amarela";
+
+if (cor === "verde"){
+    console.log("siga");
+
+} else if (cor === "amarela"){
+    console.log("espere");
+
+} else {
+    console.log("pare");
+}
---
+++ Traffic lights/index2.js
@@ -0,0 +1,20 @@
+let cor ="azul";
+
+switch (cor) {
+
+    case "verde":
+        console.log("go");
+        break;
+
+    case "amarelo":
+        console.log("wait");
+        break;
+
+    case "vermelho":
+        console.log("stop");
+        break;
+        
+    default:
+        console.log(" sistem problem ");
+
+}
